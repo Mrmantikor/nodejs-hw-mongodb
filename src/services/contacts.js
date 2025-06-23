718d6990c762166c4db936f61c8b04cefde8fea8
Vasylych <war.mant1kor@gmail.com>
2025-06-23 17:02:42 +0300

    GoIT Node Home Work №4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,62 @@
 import { ContactCollection } from '../db/models/Contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getContact = async () => {
-  return await ContactCollection.find();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactCollection.find();
+  const contactsCount = await ContactCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
-export const getContactById = async (productId) => {
-  return await ContactCollection.findById(productId);
+
+export const getContactById = async (contactId) => {
+  const contact = await ContactCollection.findById(contactId);
+
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+export const deleteContactById = async (contactId) => {
+  const contact = await ContactCollection.findOneAndDelete({
+    _id: contactId,
+  });
+
+  return contact;
+};
+
+export const updateContactById = async (contactId, payload) => {
+  const rawResult = await ContactCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+    },
+  );
+
+  return rawResult;
 };
